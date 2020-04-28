1d83b4c58727d1c986e8aa546c7fdf641c9a50da
ruscat <rus.reg.36@gmail.com>
2020-04-28 14:55:41 +0300

    разбил по папкам controllers and routes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const util = require('util');
 const morgan  = require('morgan');
+const articleController = require('./controllers/articleController');
+const userController = require('./controllers/userController');
 
 const app = express();
 app.use(express.json());
@@ -10,89 +12,35 @@ app.use((req, res, next)=>{
     next();
 });
 app.use(morgan('dev'));
-
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
+//controllers*************************************************************
 
-const getAllArticles = async (req, res)=>{
-    const articles = JSON.parse(await  readFile(`${__dirname}/data/articles.json`, 'utf-8'));
-    res.status(200).json({
-        status: 'success',
-        results: articles.length,
-        data: {
-            articles
-        }
-    })};
-const getArticle = async (req, res) =>{
-    const title = req.params.title;
-    const body= req.body;
-    const articles = JSON.parse(await  readFile(`${__dirname}/data/articles.json`, 'utf-8'));
-    let article= articles.find((item, index, arr)=>{
-        return item.title === title;
-    });
-    res.status(201).json({
-        status: 'success',
-        data:{
-            article
-        }
-    })
-};
-const createArticle = async (req, res)=>{
-    const article = req.body;
-    const articles = JSON.parse(await  readFile(`${__dirname}/data/articles.json`, 'utf-8'));
-    articles.push(article);
-    await  writeFile(`${__dirname}/data/articles.json`, JSON.stringify(articles));
-    res.status(201).json({
-        status: 'success post',
-        data: null
-    });
-};
-const updateArticle = async (req, res) =>{
-    const title = req.params.title;
-    const body= req.body;
-    const articles = JSON.parse(await  readFile(`${__dirname}/data/articles.json`, 'utf-8'));
-    let article= articles.find((item, index, arr)=>{
-        return item.title === title;
-    });
-    const index= articles.indexOf(article);
-    let newArticle = {...article, ...body};
-    articles.splice(index, 1, newArticle);
-    await  writeFile(`${__dirname}/data/articles.json`, JSON.stringify(articles));
-    res.status(201).json({
-        status: 'success',
-        data:{
-            newArticle
-        }
-    })
-};
-const deleteArticle =  async(req, res)=>{
-    const title = req.params.title;
-    const articles = JSON.parse(await  readFile(`${__dirname}/data/articles.json`, 'utf-8'));
-    let index= articles.findIndex((item, index, arr)=>{
-        return item.title === title;
-    });
-    if(index === undefined) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'not found title',
-        });
-    } else {
-        articles.splice(index, 1);
-        await  writeFile(`${__dirname}/data/articles.json`, JSON.stringify(articles));
-        res.status(204).json({
-            status: 'success',
-            data: null,
-        });
-    }
-};
 
-app.route('/api/v1/articles')
-    .get(getAllArticles)
-    .post(createArticle);
-app.route('/api/v1/articles/:title')
-    .get(getArticle)
-    .patch(updateArticle)
-    .delete(deleteArticle);
+
+
+//routers*******************************************************************
+const articleRouter = express.Router();
+const userRouter = express.Router();
+userRouter
+    .route('/')
+    .get(userController.getAllUsers)
+    .post(userController.createUser);
+app.route('/:id')
+    .get(userController.getUser)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
+
+articleRouter.route('/')
+    .get(articleController.getAllArticles)
+    .post(articleController.createArticle);
+articleRouter.route('/:title')
+    .get(articleController.getArticle)
+    .patch(articleController.updateArticle)
+    .delete(articleController.deleteArticle);
+
+app.use('/api/v1/articles', articleRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = 3000;
 app.listen(port, ()=>{
---
+++ controllers/articleController.js
@@ -0,0 +1,77 @@
+const fs= require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+exports.getAllArticles = async (req, res)=>{
+    const articles = JSON.parse(await  readFile(`${__dirname}/../data/articles.json`, 'utf-8'));
+    res.status(200).json({
+        status: 'success',
+        results: articles.length,
+        data: {
+            articles
+        }
+    })};
+exports.getArticle = async (req, res) =>{
+    const title = req.params.title;
+    const body= req.body;
+    const articles = JSON.parse(await  readFile(`${__dirname}/../data/articles.json`, 'utf-8'));
+    let article= articles.find((item, index, arr)=>{
+        return item.title === title;
+    });
+    res.status(201).json({
+        status: 'success',
+        data:{
+            article
+        }
+    })
+};
+exports.createArticle = async (req, res)=>{
+    const article = req.body;
+    const articles = JSON.parse(await  readFile(`${__dirname}/../data/articles.json`, 'utf-8'));
+    articles.push(article);
+    await  writeFile(`${__dirname}/../data/articles.json`, JSON.stringify(articles));
+    res.status(201).json({
+        status: 'success post',
+        data: null
+    });
+};
+exports.updateArticle = async (req, res) =>{
+    const title = req.params.title;
+    const body= req.body;
+    const articles = JSON.parse(await  readFile(`${__dirname}/../data/articles.json`, 'utf-8'));
+    let article= articles.find((item, index, arr)=>{
+        return item.title === title;
+    });
+    const index= articles.indexOf(article);
+    let newArticle = {...article, ...body};
+    articles.splice(index, 1, newArticle);
+    await  writeFile(`${__dirname}/../data/articles.json`, JSON.stringify(articles));
+    res.status(201).json({
+        status: 'success',
+        data:{
+            newArticle
+        }
+    })
+};
+exports.deleteArticle =  async(req, res)=>{
+    const title = req.params.title;
+    const articles = JSON.parse(await  readFile(`${__dirname}/../data/articles.json`, 'utf-8'));
+    let index= articles.findIndex((item, index, arr)=>{
+        return item.title === title;
+    });
+    if(index === undefined) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'not found title',
+        });
+    } else {
+        articles.splice(index, 1);
+        await  writeFile(`${__dirname}/../data/articles.json`, JSON.stringify(articles));
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    }
+};
