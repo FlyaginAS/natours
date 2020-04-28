2771a1c4c8258a8b73209f1701e36d2f889223e1
ruscat <rus.reg.36@gmail.com>
2020-04-28 10:22:28 +0300

    начинаю express

--- replace.js
+++
@@ -1,7 +0,0 @@
-function replacetemp(str1, obj) {
-    let page = str1.replace(/{%PRODUCTNAME%}/g, obj.name);
-    page = page.replace(/{%QUANTITY%}/g, obj.quantity);
-    page = page.replace(/{%PRICE%}/g, obj.price);
-    return page;
-}
-module.exports = replacetemp;
