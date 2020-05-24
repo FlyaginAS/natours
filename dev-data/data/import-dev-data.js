6811d81036b379336485ab65fa94d0c6374c5b8c
ruscat <rus.reg.36@gmail.com>
2020-05-24 14:04:11 +0300

    закончил 093

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const Tour = require('./../../models/tourModel');
+const Tour = require('../../models/tourModel');
 
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  '<password>',
   process.env.DATABASE_PASSWORD
 );
 
