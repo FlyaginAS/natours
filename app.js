321dd90756b883181432db3064df936bc0374b4f
ruscat <rus.reg.36@gmail.com>
2020-05-27 14:41:08 +0300

    закончил 116

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const  express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouters');
 const userRouter = require('./routes/userRouters');
 
@@ -13,5 +15,9 @@ app.use(express.static(`${__dirname}/public`));
 //routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+app.use(globalErrorHandler);
 
 module.exports = app;
