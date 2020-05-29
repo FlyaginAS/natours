19b9c94a16a56df0f687dafd21c24799e131c0cc
ruscat <rus.reg.36@gmail.com>
2020-05-29 15:39:29 +0300

    122

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+
+process.on('uncaughtException', (err) => {
+  console.log('ERROR',err.name, err.message);
+  console.log('shutting down');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
