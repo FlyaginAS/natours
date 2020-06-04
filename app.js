6033d59390eb5fa4443fa9951fb87fde0a9684a2
ruscat <rus.reg.36@gmail.com>
2020-06-04 15:55:04 +0300

    143

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,20 +11,25 @@ const userRouter = require('./routes/userRouters');
 const app = express();
 
 // 1) MIDDLEWARES
+//secutity http headers
+app.use(helmet());
+//development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+//limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60*60*1000,
   message: 'Too many request from this IP in hour',
 });
 app.use('/api', limiter);
-
-app.use(express.json());
+//body parser, reading data from body to req.body
+//ограничим размер body
+app.use(express.json({ limit: '10kb' }));
+//serving static files
 app.use(express.static(`${__dirname}/public`));
-
+//my test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
