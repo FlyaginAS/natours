06aaf88f0103e12157640625a97531c93b057ba5
ruscat <rus.reg.36@gmail.com>
2020-06-04 10:19:20 +0300

    138

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,14 +6,14 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
 
-const signToken = async (id) => {
+const signToken =  (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
 
-const  createSendToken = (user, statusCode, res) => {
-  const token = signToken(user._id);
+const  createSendToken =  (user, statusCode, res) => {
+  const token =  signToken(user._id);
   res.status(statusCode).json({
     status: 'success',
     token,
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,16 +1,25 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+}
 
 exports.getAllUsers = async (req, res, next) => {
- const users = await User.find();
+  const users = await User.find();
 
- res.status(200).json({
-   status: 'success',
-   results: users.length,
-   data: {
-     users,
-   },
- });
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
 };
 exports.getUser = (req, res)=>{
   res.status(500).json({
@@ -36,3 +45,25 @@ exports.createUser = (req, res)=>{
     message: 'This route is not yet defined'
   });
 };
+exports.updateMe = catchAsync( async (req, res, next) => {
+  //1) create error if user Posts password data
+  if(req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates. Use /updateMyPassword', 400));
+  }
+
+  //2) filtered out unwanted fields
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  //3) update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+  data: {
+      user: updatedUser,
+  },
+  });
+});
