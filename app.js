fd80b7c039b7b5645b311468fc04daa7a77adb88
ruscat <rus.reg.36@gmail.com>
2020-06-04 17:06:11 +0300

    145

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +34,10 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 //data sanitization against XSS
 app.use(xss());
+//prevent parameter pollution
+app.use(hpp({
+  whitelist: ['duration']
+}));
 //serving static files
 app.use(express.static(`${__dirname}/public`));
 //my test middleware
