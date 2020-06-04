faad5a69a17de08e4247b9f6777c0e7a7a4affc1
ruscat <rus.reg.36@gmail.com>
2020-06-04 16:34:24 +0300

    144

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +29,10 @@ app.use('/api', limiter);
 //body parser, reading data from body to req.body
 //ограничим размер body
 app.use(express.json({ limit: '10kb' }));
+//data sanitization against NoSQL injection
+app.use(mongoSanitize());
+//data sanitization against XSS
+app.use(xss());
 //serving static files
 app.use(express.static(`${__dirname}/public`));
 //my test middleware
