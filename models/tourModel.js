58a3b3ee6991fb114264d77b30fac78f9f0a1af2
ruscat <rus.reg.36@gmail.com>
2020-06-06 13:35:22 +0300

    151

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -89,7 +89,12 @@ const tourSchema = new mongoose.Schema({
         day: Number,
       },
     ],
-  guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   //option object
   {
@@ -107,11 +112,11 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 //post middleware
 // tourSchema.post('save', function(doc, next) {
