9d237a393d15b5107b12aa93071b117e7c2ae83d
ruscat <rus.reg.36@gmail.com>
2020-06-17 09:40:44 +0300

    181

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //1) get tour data from collection
+  const tours = await Tour.find();
+  //2)build template
+
+  //3) render this template using tour data from 1)
+
   res.status(200).render('overview', {
     title: 'All tours',
+    tours,
   });
-};
-exports.getTour = (req, res) =>{
+});
+exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The forest Hiker Tour',
   });
