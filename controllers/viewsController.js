5945640ce4d0bf1307d82a307904b34915a257eb
ruscat <rus.reg.36@gmail.com>
2020-06-18 11:57:52 +0300

    185

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -25,7 +25,7 @@ exports.getTour = catchAsync( async (req, res) => {
   //3) render template using data from 1)
 
   res.status(200).render('tour', {
-    title: 'The forest Hiker Tour',
+    title: `${tour.name} tour`,
     tour,
   });
 });
