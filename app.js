a6e93e0ef20aaf4fa0edc165cf8fdb833b965301
ruscat <rus.reg.36@gmail.com>
2020-06-22 11:22:43 +0300

    188

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -38,6 +39,7 @@ app.use('/api', limiter);
 //body parser, reading data from body to req.body
 //ограничим размер body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 //data sanitization against NoSQL injection
 app.use(mongoSanitize());
 //data sanitization against XSS
@@ -50,7 +52,7 @@ app.use(hpp({
 //my test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 
