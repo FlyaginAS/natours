601f79c43d24a75c619e39354e61a43605cebcc4
ruscat <rus.reg.36@gmail.com>
2020-06-26 13:49:56 +0300

    204

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -24,10 +24,38 @@ exports.uploadTourImages = upload.fields([
   {name: 'imageCover', maxCount: 1},
   {name: 'images', maxCount: 3},
 ]);
-exports.resizeTourImages = (req, res, next) => {
-  console.log(req.files);
+
+exports.resizeTourImages = catchAsync( async (req, res, next) => {
+  if(!req.files.imageCover || !req.files.images) return next();
+  //1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000,1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+  //этот код будет вызван перед  tourControllers.updateTour, а ему нужен req.body-боди пойдет в базу обновлять
+
+  //2) Images
+  //map нужен, чтобы получить массив промисов, которые мы долждемся до результата
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i+1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000,1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
   next();
-};
+});
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
