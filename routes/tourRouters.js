d3388902d9ef3ab91aff8aec3ba15c60d34251c2
ruscat <rus.reg.36@gmail.com>
2020-06-26 12:40:24 +0300

    203

--- routes/tourRouters.js
+++ routes/tourRouters.js
@@ -50,6 +50,8 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourControllers.uploadTourImages,
+    tourControllers.resizeTourImages,
     tourControllers.updateTour
   );
 
