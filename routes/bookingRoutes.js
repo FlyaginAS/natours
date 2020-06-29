e2c6de66a1b67e73e3206cf26987ad2efb2e0ab4
ruscat <rus.reg.36@gmail.com>
2020-06-29 09:32:23 +0300

    215

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const bookingController = require('../controllers/bookingController');
-const  authController = require('../controllers/authController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
-//не соблюдаем именование rest так как  задача просто организовать сессию
-router.get(
-  '/checkout-session/:tourId',
-  authController.protect,
-  bookingController.getCheckoutSession
-);
-module.exports = router;
 
+router.use(authController.protect);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
+
+module.exports = router;
